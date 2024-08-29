2b5a7e46a8f5f9fb41921c1d777f87ccdfcac2c2
amitg042 <amit.b.g@outlook.com>
2024-08-29 11:05:02 +0000

    test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddRazorPages();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthorization();
+
+// app.MapRazorPages();
+
+// app.Run();
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,6 +48,17 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Map the default Razor Pages
 app.MapRazorPages();
+
+// Map the /api/products route to serve the same content as /
+app.Map("/api/products", builder =>
+{
+    builder.UseRouting();
+    builder.UseEndpoints(endpoints =>
+    {
+        endpoints.MapRazorPages(); // Map the Razor Pages to this path
+    });
+});
 
 app.Run();
